6db5446a32c786e0b67da5979b0bf42da2efe454
Vaibhav Singh <88562515+NewEthicker@users.noreply.github.com>
2023-05-14 20:06:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+import cv2
+import mediapipe as mp
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+
+url = "https://10.55.81.38:8080/video"
+cap = cv2.VideoCapture(url)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = HandDetector(maxHands=1, detectionCon=0.6)
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+finalText = ""
+prev_button = None
+
+def drawAll(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+    return img
+
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+
+buttonList = []
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bboxInfo = detector.findPosition(img)
+    img = drawAll(img, buttonList)
+
+    if lmList:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+
+            if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
+                cv2.rectangle(img, button.pos, (x + w, y + h), (175, 0, 175), cv2.FILLED)
+                cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                l, _, _ = detector.findDistance(8, 12, img, draw=False)
+                print(l)
+
+                if prev_button != button:  # Check if the previous button is not the same as the current button
+                    if l < 50:
+                        cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
+                        cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                        finalText += button.text
+                        prev_button = button  # Update the previous button to the current button
+                        sleep(0.3)
+
+    cv2.rectangle(img, (100, 350), (990, 450), (255, 255, 30), cv2.FILLED)
+    cv2.putText(img, finalText, (100, 425), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
